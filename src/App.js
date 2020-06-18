801d46833f813a1c4bdfc98d7dc5fdeae12423a9
sir confy <csirconfy@gmail.com>
2020-06-18 06:48:04 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import advice from './advice.jpg'
+import axios from 'axios'
 
-function App() {
+
+class App extends React.Component{
+  constructor() {
+  super() 
+   this.state = {
+     advice: ''
+     }
+  }
+componentDidMount() {
+this.fetchAdvice();
+}
+ fetchAdvice = () => {
+ axios.get('https://api.adviceslip.com/advice')
+   .then((response) => {
+     const {advice} = response.data.slip;
+     this.setState({advice: advice});
+   })
+   .catch((error) => {
+     console.log(error)
+   });
+  
+  }
+ 
+
+  render() {
+    const {advice} = this.state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+       
+        <button className="button" onClick= {this.fetchAdvice} >CLICK ME TO ADVICE YOU</button>
+      </div>
+    <h2><marquee  width="0"direction="right">MAKE SURE YOU TAKE THE ADVICE SERIOUS: SirConfy Cares</marquee></h2>
     </div>
   );
+  }
 }
 
 export default App;
